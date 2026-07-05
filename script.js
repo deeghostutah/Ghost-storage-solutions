363719e7e00f60817c8dc73f0144f90bda60b835
deeghostutah <dee@ghoststoragesolutions.com>
2026-07-05 15:49:24 -0600

    mobile navigation menu

--- script.js
+++ script.js
@@ -22,3 +22,12 @@ if (projectForm) {
     }
   });
 }
+
+const menuToggle = document.querySelector(".menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+
+if (menuToggle && navLinks) {
+  menuToggle.addEventListener("click", function () {
+    navLinks.classList.toggle("open");
+  });
+}
